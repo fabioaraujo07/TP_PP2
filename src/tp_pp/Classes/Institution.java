d56376c766cc43b27472695fb7e3e77b1f3c4823
Roger Nakauchi <Roger Nakauchi@DESKTOP-KHR6E9H>
2024-05-29 21:34:01 +0100

    Some parts of Container class and Measurement class as well

---
+++ src/tp_pp/Classes/Institution.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp_pp.Classes;
+
+import com.estg.core.AidBox;
+import com.estg.core.Container;
+import com.estg.core.ItemType;
+import com.estg.core.Measurement;
+import com.estg.core.exceptions.AidBoxException;
+import com.estg.core.exceptions.ContainerException;
+import com.estg.core.exceptions.MeasurementException;
+import com.estg.core.exceptions.PickingMapException;
+import com.estg.core.exceptions.VehicleException;
+import com.estg.pickingManagement.PickingMap;
+import com.estg.pickingManagement.Vehicle;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Roger Nakauchi
+ */
+public class Institution implements com.estg.core.Institution { 
+    
+    private String name;    
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean addAidBox(AidBox aidbox) throws AidBoxException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean addMeasurement(Measurement msrmnt, Container cntnr) throws ContainerException, MeasurementException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public AidBox[] getAidBoxes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Container getContainer(AidBox aidbox, ItemType it) throws ContainerException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Vehicle[] getVehicles() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean addVehicle(Vehicle vhcl) throws VehicleException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void disableVehicle(Vehicle vhcl) throws VehicleException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void enableVehicle(Vehicle vhcl) throws VehicleException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public PickingMap[] getPickingMaps() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public PickingMap[] getPickingMaps(LocalDateTime ldt, LocalDateTime ldt1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public PickingMap getCurrentPickingMap() throws PickingMapException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean addPickingMap(PickingMap pm) throws PickingMapException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double getDistance(AidBox aidbox) throws AidBoxException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
