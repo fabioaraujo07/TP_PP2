dc6c273ed2ce61c8806bd2cb99d3650a236d8bfa
Roger Nakauchi <Roger Nakauchi@DESKTOP-KHR6E9H>
2024-06-01 09:53:46 +0100

    Starting Route class (in progress)

--- src/tp_pp_managment/RouteImp.java
+++ src/tp_pp_managment/RouteImp.java
@@ -14,8 +14,8 @@ import tp_pp_exceptions.FindException;
  *
  * @author fabio
  */
-public class RouteImp implements com.estg.pickingManagement.Route{
-    
+public class RouteImp implements com.estg.pickingManagement.Route {
+
     private AidBox[] routes;
     private int numberAidboxes;
     private Vehicle vehicle;
@@ -24,82 +24,154 @@ public class RouteImp implements com.estg.pickingManagement.Route{
         this.routes = new AidBox[10];
         this.numberAidboxes = 0;
     }
-    
-    
+
     public boolean findAidBox(AidBox aidbox) {
-        for(int i = 0; i < numberAidboxes; i++) {
-            if(routes[i].equals(aidbox)) {
+        for (int i = 0; i < numberAidboxes; i++) {
+            if (routes[i].equals(aidbox)) {
                 return true;
             }
         }
         return false;
     }
-    
-    
+
     @Override
     public void addAidBox(AidBox aidbox) throws RouteException {
-        
-        if(aidbox == null) {
+
+        if (aidbox == null) {
             throw new RouteException("Aidbox can't be null");
         }
-        if(findAidBox(aidbox)) {
+        if (findAidBox(aidbox)) {
             throw new RouteException("Aidbox already exists in the route");
         }
-        if(vehicle instanceof VehicleImp) {
-            if(!((VehicleImp) vehicle).canTransport(ItemType.CLOTHING)) {
+        if (vehicle instanceof VehicleImp) {
+            if (!((VehicleImp) vehicle).canTransport(ItemType.CLOTHING)) {
                 throw new RouteException("Vehicle can't transport Clothing Aidbox type");
             }
-            if(!((VehicleImp) vehicle).canTransport(ItemType.MEDICINE)) {
+            if (!((VehicleImp) vehicle).canTransport(ItemType.MEDICINE)) {
                 throw new RouteException("Vehicle can't transport Medicine Aidbox type");
             }
-            if(!((VehicleImp) vehicle).canTransport(ItemType.NON_PERISHABLE_FOOD)) {
+            if (!((VehicleImp) vehicle).canTransport(ItemType.NON_PERISHABLE_FOOD)) {
                 throw new RouteException("Vehicle can't transport Non Perishable Food Aidbox type");
             }
-            if(!((VehicleImp) vehicle).canTransport(ItemType.PERISHABLE_FOOD)) {
+            if (!((VehicleImp) vehicle).canTransport(ItemType.PERISHABLE_FOOD)) {
                 throw new RouteException("Vehicle can't transport Perishable food Aidbox type");
             }
         }
-        routes[numberAidboxes++] = aidbox;        
+        routes[numberAidboxes++] = aidbox;
     }
 
     @Override
     public AidBox removeAidBox(AidBox aidbox) throws RouteException {
-        
+
         AidBox removedAidbox = null;
-        
-        if(aidbox == null) {
+
+        if (aidbox == null) {
             throw new RouteException("Aidbox can't be null");
         }
-        if(findAidBox(aidbox) == false) {
+        if (findAidBox(aidbox) == false) {
             throw new RouteException("Aidbox could not be found");
         }
-        for(int i = 0; i < numberAidboxes; i++) {
-            if(routes[i].equals(aidbox)) {
+        for (int i = 0; i < numberAidboxes; i++) {
+            if (routes[i].equals(aidbox)) {
                 removedAidbox = routes[i];
             }
-            
-            for(int j = i; j < numberAidboxes - 1; j++) {
+
+            for (int j = i; j < numberAidboxes - 1; j++) {
                 routes[j] = routes[j + 1];
             }
             routes[numberAidboxes - 1] = null;
-            numberAidboxes--;            
+            numberAidboxes--;
         }
         return removedAidbox;
     }
 
     @Override
     public boolean containsAidBox(AidBox aidbox) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (findAidBox(aidbox)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void replaceAidBox(AidBox aidbox, AidBox aidbox1) throws RouteException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        if (aidbox == null) {
+            throw new RouteException("Any AidBox can´t be null");
+        }
+        if (aidbox1 == null) {
+            throw new RouteException("Any AidBox can´t be null");
+        }
+        if (findAidBox(aidbox) == false) {
+            throw new RouteException("AidBox to replace is not in the route");
+        }
+        if (findAidBox(aidbox1) == true) {
+            throw new RouteException("AidBox to insert is already in the route");
+        }
+        if (vehicle instanceof VehicleImp) {
+            if (!((VehicleImp) vehicle).canTransport(ItemType.CLOTHING)) {
+                throw new RouteException("Vehicle can't transport Clothing Aidbox type");
+            }
+            if (!((VehicleImp) vehicle).canTransport(ItemType.MEDICINE)) {
+                throw new RouteException("Vehicle can't transport Medicine Aidbox type");
+            }
+            if (!((VehicleImp) vehicle).canTransport(ItemType.NON_PERISHABLE_FOOD)) {
+                throw new RouteException("Vehicle can't transport Non Perishable Food Aidbox type");
+            }
+            if (!((VehicleImp) vehicle).canTransport(ItemType.PERISHABLE_FOOD)) {
+                throw new RouteException("Vehicle can't transport Perishable food Aidbox type");
+            }
+        }
+
+        for (int i = 0; i < numberAidboxes; i++) {
+            if (routes[i].equals(aidbox)) {
+                routes[i] = aidbox1;
+            }
+        }
+
     }
 
     @Override
     public void insertAfter(AidBox aidbox, AidBox aidbox1) throws RouteException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        if (aidbox == null) {
+            throw new RouteException("AidBox to insert before can´t be null");
+        }
+        if (aidbox1 == null) {
+            throw new RouteException("AidBox to insert can´t be null");
+        }
+        if (findAidBox(aidbox1) == true) {
+            throw new RouteException("AidBox is already in the route");
+        }
+        if (vehicle instanceof VehicleImp) {
+            if (!((VehicleImp) vehicle).canTransport(ItemType.CLOTHING)) {
+                throw new RouteException("Vehicle can't transport Clothing Aidbox type");
+            }
+            if (!((VehicleImp) vehicle).canTransport(ItemType.MEDICINE)) {
+                throw new RouteException("Vehicle can't transport Medicine Aidbox type");
+            }
+            if (!((VehicleImp) vehicle).canTransport(ItemType.NON_PERISHABLE_FOOD)) {
+                throw new RouteException("Vehicle can't transport Non Perishable Food Aidbox type");
+            }
+            if (!((VehicleImp) vehicle).canTransport(ItemType.PERISHABLE_FOOD)) {
+                throw new RouteException("Vehicle can't transport Perishable food Aidbox type");
+            }
+        }
+
+        int positionAidBox1 = -1;
+        for (int i = 0; i < numberAidboxes; i++) {
+            if(routes[i].equals(positionAidBox1)) {
+                positionAidBox1 = i;
+            }
+        }
+        
+        for(int i = numberAidboxes; i > positionAidBox1; i--) {
+            routes[i] = routes[i - 1];
+        }
+        
+        routes[positionAidBox1 + 1] = aidbox1;
+        numberAidboxes++;
+
     }
 
     @Override
@@ -121,5 +193,5 @@ public class RouteImp implements com.estg.pickingManagement.Route{
     public double getTotalDuration() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
